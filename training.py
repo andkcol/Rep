34c80baab6e0a2c3620686afcda1f6ffa332cfd4
andkcol <95748467+andkcol@users.noreply.github.com>
2024-12-13 11:02:29 +0000

    Update training.py

--- training.py
+++ training.py
@@ -4,7 +4,7 @@
 import pandas as pd
 
 # Load dataset
-data = pd.read_csv('trainingdata.csv')
+data = pd.read_csv('AAPL1980.csv')
 
 # Inspect first few rows, ensure data is read correctly
 print(data.head())
@@ -13,11 +13,11 @@ print(data.head())
 data = data.dropna()
 
 # Calculate moving averages
-data['shortMA'] = data['Close'].rolling(window = 13).mean()
-data['longMA'] = data['Close'].rolling(window = 21).mean()
+data['shortMA'] = data['close'].rolling(window = 13).mean()
+data['longMA'] = data['close'].rolling(window = 21).mean()
 
 # Inspect data
-print(data[['Close', 'shortMA', 'longMA']].tail())
+print(data[['close', 'shortMA', 'longMA']].tail())
 
 # Create signal
 data['Signal'] = 0
@@ -25,7 +25,7 @@ data.loc[data['shortMA'] > data['longMA'], 'Signal'] = 1 #BUY
 data.loc[data['shortMA'] <= data['longMA'], 'Signal'] = -1 #Sell
 
 # Ispect signals
-print(data[['Close', 'shortMA', 'longMA', 'Signal']].tail())
+print(data[['close', 'shortMA', 'longMA', 'Signal']].tail())
 
 # BACKTESTING
 # initialise backtesting variables
@@ -36,15 +36,15 @@ data['Portfolio'] = 0 #Track portfolio value
 
 for i in range(2, len(data)):
     if data['Signal'].iloc[i] == 1 and positions == 0: #Buy signal
-        positions = balance / data['Close'].iloc[i] #Buy as many shares as possible
+        positions = balance / data['close'].iloc[i] #Buy as many shares as possible
         balance = 0
     elif data['Signal'].iloc[i] == -1 and positions > 0: #Sell signal
-        balance = positions * data['Close'].iloc[i] #Sell all shares
+        balance = positions * data['close'].iloc[i] #Sell all shares
         positions = 0
 
     # Update portfolio value
-    data['Portfolio'].iloc[i] = balance + (positions * data['Close'].iloc[i])
+    data['Portfolio'].iloc[i] = balance + (positions * data['close'].iloc[i])
 
 # Final portfolio value
-final_balance = balance + (positions * data['Close'].iloc[-1])
+final_balance = balance + (positions * data['close'].iloc[-1])
 print(f"Final portfolio value: {final_balance}")
